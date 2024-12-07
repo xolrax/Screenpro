01177c8259b4ac44bdf50ad345c55524503019fa
xolrax <xalvear@gmail.com>
2024-12-07 16:27:26 -0300

    Delete pruebas directory

--- pruebas/Pruebas.py
+++
@@ -1,8 +0,0 @@
-import os
-import inspect
-from dotenv import load_dotenv
-
-ruta_archivo = os.path.abspath(inspect.getfile(inspect.currentframe()))
-base_dir = os.path.dirname(ruta_archivo)
-
-print(f"ruta de archivo : {base_dir}")
